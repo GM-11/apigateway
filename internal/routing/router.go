87a8a04b15b4a65c6a7cc3e16ac4bcc6671a0925
Gopal Mathur <mathuyrgopal@gmail.com>
2026-04-15 23:33:00 +0530

    Using reverse proxy instead of manual request sending.

--- internal/routing/router.go
+++ internal/routing/router.go
@@ -1,13 +1,11 @@
 package routing
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
+	"net/http/httputil"
 
 	"example.com/m/v2/internal/auth"
+	"github.com/google/uuid"
 )
 
 type Router struct {
@@ -16,27 +14,20 @@ type Router struct {
 	client     *http.Client
 }
 
-type authHeaderKeyType string
+func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	route := router.trie.Match(path)
+	if route == nil {
+		http.Error(w, "Path does not exist", http.StatusNotFound)
+		return
+	}
 
-const authHeaderKey authHeaderKeyType = "authHeader"
+	if route.AuthRequired {
+		auth.Middleware(router.serve(route, path)).ServeHTTP(w, r)
+	} else {
+		router.serve(route, path).ServeHTTP(w, r)
+	}
 
-func (router *Router) ServeHTTP(path string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		route := router.trie.Match(path)
-		if route == nil {
-			http.Error(w, "Path does not exist", http.StatusBadRequest)
-			return
-		}
-
-		if route.AuthRequired {
-			authHeader := r.Header.Get("Authorization")
-			ctx := context.WithValue(r.Context(), authHeaderKey, authHeader)
-			auth.Middleware(router.serve(route, path)).ServeHTTP(w, r.WithContext(ctx))
-		} else {
-			router.serve(route, path).ServeHTTP(w, r)
-		}
-
-	})
 }
 
 func (router *Router) serve(route *Route, path string) http.Handler {
@@ -44,54 +35,18 @@ func (router *Router) serve(route *Route, path string) http.Handler {
 		selectedStreamIndex := router.roundRobin.GetServerIndex(*route)
 		selectedStream := route.Upstreams[selectedStreamIndex]
 
-		url := fmt.Sprintf("%s%s", selectedStream, path)
+		requestId := uuid.New().String()
 
-		req, err := http.NewRequest(r.Method, url, r.Body)
-
-		if err != nil {
-			log.Println(err.Error())
-			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		proxy := &httputil.ReverseProxy{
+			Director: func(req *http.Request) {
+				req.URL.Scheme = "http"
+				req.URL.Host = selectedStream
+				req.URL.Path = path
+				req.Header.Set("X-Request-ID", requestId)
+			},
 		}
 
-		if route.AuthRequired {
-			authHeader := r.Context().Value(authHeaderKey).(string)
-			if len(authHeader) == 0 {
-				http.Error(w, "Something Went Wrong", http.StatusInternalServerError)
-				return
-			}
-			req.Header.Add("Authorization", authHeader)
-		}
-
-		for header, values := range r.Header {
-			for _, value := range values {
-				req.Header.Add(header, value)
-			}
-		}
-
-		resp, err := router.client.Do(req)
-		if err != nil {
-			log.Println(err)
-			http.Error(w, "Something went wrong", http.StatusInternalServerError)
-			return
-		}
-		defer resp.Body.Close()
-
-		for header, values := range resp.Header {
-			for _, value := range values {
-				w.Header().Add(header, value)
-			}
-		}
-
-		var response any
-
-		json.NewDecoder(resp.Body).Decode(&response)
-		w.WriteHeader(resp.StatusCode)
-		err = json.NewEncoder(w).Encode(response)
-		if err != nil {
-			log.Println(err)
-			http.Error(w, "Error encoding json", http.StatusInternalServerError)
-			return
-		}
+		proxy.ServeHTTP(w, r)
 	})
 
 }
