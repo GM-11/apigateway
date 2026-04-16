c1e2e9ac48c694e649a2c10871a10eeb60e4b54e
Gopal Mathur <mathuyrgopal@gmail.com>
2026-04-16 12:55:23 +0530

    Implemented token bucket

---
+++ internal/ratelimitting/tokenbucket.go
@@ -0,0 +1,30 @@
+package ratelimitting
+
+import (
+	"sync"
+	"time"
+)
+
+type TokenBucket struct {
+	mu         sync.Mutex
+	tokens     float64
+	lastRefill time.Time
+	refillRate float64
+	maxTokens  float64
+}
+
+func (tb *TokenBucket) Allow() bool {
+	tb.mu.Lock()
+	defer tb.mu.Unlock()
+	timeElapsed := time.Since(tb.lastRefill)
+	tb.tokens += timeElapsed.Seconds() * tb.refillRate
+
+	tb.tokens = min(tb.tokens, tb.maxTokens)
+
+	tb.lastRefill = time.Now()
+	if tb.tokens >= 1 {
+		tb.tokens -= 1
+		return true
+	}
+	return false
+}
